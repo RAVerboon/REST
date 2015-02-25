af415d07c68c15487c4d9447038c66d7fd6fcbba
Gert-Jan van Ginkel <gertjanvg@gmail.com>
2015-02-25 23:07:17 +0100

    jQuery UI sucks

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 		<meta charset="UTF-8">
 		<!--jQuery-->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<link rel="stylesheet" href="./stylesheet/jquery-ui.css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
 	</head>
 	<body>
 		<header id="home">
@@ -16,7 +18,7 @@
 				<ul id='headerList'>
 					<li><a href="#home" id="homeLink">Home</a></li>
 					<li class="seperator"></li>
-					<li><a href="#" id="whatLink">Wat is REST?</a></li>
+					<li><a href="#what" id="whatLink">Wat is REST?</a></li>
 					<li class="seperator"></li>
 					<li><a href="#" id="altLink">Alternatieven</a></li>
 					<li class="seperator"></li>
@@ -62,32 +64,29 @@
 			</div>
 		</header>
 		<main>
-		
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean est elit, faucibus vel est non, dictum commodo risus. Vivamus varius dignissim turpis, at pulvinar neque dignissim ac. Donec egestas dapibus porttitor. Fusce sit amet volutpat enim. Nulla facilisi. Suspendisse massa tortor, laoreet sed mauris in, fringilla porttitor elit. Vestibulum consectetur mi in tincidunt tristique. Donec bibendum justo nec nisi feugiat vulputate. Aliquam erat volutpat. Phasellus in quam sed lacus gravida ornare quis a sem.
-
-Nulla facilisi. In ultricies lobortis lacus id vulputate. Integer turpis mi, feugiat a faucibus eu, iaculis viverra magna. Phasellus mollis facilisis volutpat. Nunc viverra et odio a egestas. Mauris hendrerit tincidunt elit et elementum. Aliquam vel tempus orci. Aenean placerat nibh vitae mollis sagittis. Vestibulum vestibulum blandit mauris nec pellentesque. Etiam in hendrerit odio. Fusce nec rhoncus eros. Quisque semper luctus rutrum. Curabitur euismod finibus nulla non fermentum.
-
-Vivamus sit amet nulla at leo elementum condimentum. Aenean posuere, odio ultrices vestibulum auctor, felis velit faucibus urna, sed tempus massa metus imperdiet dolor. Sed sed elit nec felis tincidunt luctus at auctor nulla. Nulla at dignissim lectus, vitae ultrices mi. Vivamus lacinia eleifend enim ut vestibulum. Sed lacinia odio urna, a efficitur neque viverra venenatis. Integer rhoncus, nulla nec dignissim imperdiet, erat orci varius mauris, et molestie quam sapien tempor odio. Nam id lorem enim. Praesent a erat dui. Aliquam erat volutpat. Integer consectetur blandit sodales. Integer in laoreet sapien, quis vehicula nibh. Pellentesque hendrerit nulla quam, sit amet scelerisque ante tempor ac. Donec non erat a nibh elementum lacinia ut sit amet purus. Phasellus laoreet lectus arcu. Donec pretium pulvinar vehicula.
-
-In hac habitasse platea dictumst. Nam sed elementum leo. Praesent luctus, sapien et malesuada commodo, sapien enim luctus nisi, et aliquet quam nisl non ante. Nulla facilisi. Duis aliquet tristique ipsum a bibendum. Integer pellentesque gravida libero et dignissim. Ut tempus ligula eros, id placerat purus aliquam eget.
-
-Phasellus maximus, arcu venenatis dictum elementum, elit massa maximus erat, ac luctus est ipsum quis quam. Nullam quis ultrices velit. Nulla feugiat tincidunt tellus, quis tincidunt metus scelerisque in. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse in ex enim. Suspendisse metus nisl, condimentum sit amet ipsum at, rutrum hendrerit massa. Praesent ligula elit, fringilla eu vehicula eu, pharetra a leo.
-
-Quisque et maximus orci. Phasellus nisi neque, semper id tortor a, venenatis consectetur magna. Curabitur maximus mauris metus, gravida pulvinar dolor volutpat pretium. Maecenas libero lacus, fringilla quis lorem non, tincidunt sagittis urna. Suspendisse porta magna lorem, in scelerisque dui tincidunt eu. Pellentesque tortor ligula, pharetra vitae rhoncus ut, dignissim quis massa. Ut tortor nibh, iaculis a porttitor at, maximus id arcu. Vestibulum sit amet eros id urna dictum tristique. Praesent eget ligula tellus. Phasellus rhoncus massa elit, ac ullamcorper elit dictum quis. Integer aliquam nisl eget nisl varius tincidunt.
-
-Sed vitae risus sem. Aliquam erat risus, aliquet in augue at, vulputate vestibulum nulla. Donec quis velit eget purus suscipit posuere. Nulla condimentum dictum felis, vitae rhoncus eros consequat eu. Donec turpis nibh, imperdiet ut interdum nec, molestie ac leo. Proin vitae elit odio. Sed laoreet felis id malesuada congue. Integer varius, justo eu ultrices dignissim, sem nibh rutrum orci, at eleifend quam arcu a augue.
-
-Maecenas lacinia iaculis ligula eget pulvinar. Suspendisse nec mauris in diam ullamcorper vulputate. Integer sit amet tempor elit, ut scelerisque ex. Phasellus nec lobortis ipsum. Vivamus sagittis scelerisque mi eu ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas quis quam sapien. Integer placerat scelerisque fermentum. In facilisis, neque quis faucibus elementum, nisl odio dignissim augue, at dapibus augue odio ut ante.
-
-Praesent porttitor id erat ut mattis. Cras porttitor bibendum enim eu congue. Morbi eu interdum odio. Cras sit amet orci fringilla, consequat ex id, imperdiet nisl. Cras vitae venenatis erat, vitae pellentesque augue. Proin nunc nulla, varius et mattis non, egestas ac urna. Sed nec justo mattis, volutpat nisl vitae, suscipit tortor. In at mauris ut nulla vestibulum varius id quis enim. Nam et dui eleifend, interdum sem ac, accumsan mauris. Morbi sit amet diam eleifend, commodo quam nec, feugiat nulla. Ut finibus cursus eros, a consectetur lectus. Integer id laoreet turpis. Praesent dapibus elit in sapien hendrerit molestie. Mauris varius tempus odio a vehicula. Praesent scelerisque nulla vel risus vehicula maximus. Etiam lobortis nunc sit amet dui sodales elementum.
-
-Mauris dui nibh, varius quis magna et, vulputate finibus diam. Ut efficitur lobortis egestas. Integer fringilla lorem tortor, quis feugiat lectus placerat nec. Ut lacinia ultrices malesuada. Fusce in faucibus lacus, in iaculis nulla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec accumsan tincidunt ex, vitae elementum augue sagittis vel. Vivamus commodo, massa in consequat tincidunt, odio risus aliquam ipsum, at laoreet ipsum neque sit amet ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam posuere at felis sed faucibus. Suspendisse pretium lorem arcu, nec lacinia velit ultricies nec. Nam ut dui eu odio fringilla bibendum. Vestibulum venenatis augue sodales leo blandit, ut aliquam mi sodales.
+			<section id="what">
+				<h1>Wat is REST?</h1>
+				<h3>Stapsgewijze opbouw REST architectuur</h3>
+				<div id="accordion">
+					<ul>
+						<li><a href="#firstHeader">First header</a></li>
+						<li><a href="#secondHeader">Second header</a></li>
+					</ul>
+					<div id="firstHeader">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed tincidunt eros, at porta massa. Mauris nec purus ac tortor fermentum elementum eget a lorem. Vivamus at vehicula ex. Vestibulum sollicitudin lacus at tempor laoreet. Proin rutrum fringilla orci a fermentum. Cras vulputate nec nisi id volutpat. Quisque hendrerit bibendum mauris, eget porta velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer feugiat hendrerit sapien. Fusce eleifend libero et nunc porttitor consequat. Aenean pulvinar id leo eget tempus.</p>
+					</div>
+					<div id="secondHeader">
+						<p>Sed sagittis rhoncus laoreet. Aenean in purus sit amet ante rutrum convallis quis fringilla orci. Quisque metus sapien, tincidunt ut venenatis quis, semper sit amet nisl. Morbi imperdiet odio gravida, blandit mauris id, tincidunt ex. Etiam a dui at eros feugiat mollis. Quisque volutpat lorem pretium, posuere tellus et, vestibulum est. Etiam eu erat finibus, tristique mauris nec, consectetur tortor. Aliquam luctus ullamcorper porttitor. Praesent gravida pulvinar scelerisque. Sed fermentum, velit in venenatis semper, nunc nisl venenatis dui, vel mattis arcu metus sed turpis. Aliquam tempor tristique odio in sodales.</p>
+					</div>
+				</div>
+			</section>
 		</main>
 		<footer>
 			
 		</footer>
 
 		<script>
+			$("#accordion").tabs();
 			$("#whatLink").hover(function() {
 				hideSubMenus();
 				$("#whatList").css('display', 'block');
