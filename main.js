46ba1192476ae94bd5709d040e4b676a2360082d
Gert-Jan van Ginkel <gertjanvg@gmail.com>
2015-03-02 15:17:49 +0100

    - JavaScript in aparte .js file gedaan, overzichtelijker.  - Flickr API toegepast, moet nog een custom search inbouwen.

--- main.js
+++ main.js
@@ -1,3 +1,75 @@
-$("#whatLink").hover(function() {
-	document.getElementById("#whatList").style.display = block;
+$(function () {
+	$("#derivation").accordion({
+		heightStyle: "content"
+	});
+	$(document).tooltip();
+
+	$("#whatLink").hover(function() {
+		hideSubMenus();
+		$("#whatList").css('display', 'block');
+		$("#whatList").addClass('animated fadeInDown');
+	});
+	$("#altLink").hover(function() {
+		hideSubMenus();
+		$("#altList").css('display', 'block');
+		$("#altList").addClass('animated fadeInDown');
+	});
+	$("#originLink").hover(function() {
+		hideSubMenus();
+		$("#originList").css('display', 'block');
+		$("#originList").addClass('animated fadeInDown');
+	});
+	$("#appLink").hover(function() {
+		hideSubMenus();
+		$("#appList").css('display', 'block');
+		$("#appList").addClass('animated fadeInDown');
+	});
+
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 400);
+	});
+	$('#scrollToTop').click(function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 400);
+	});
+	$(document).scroll(function() {
+		var y = $(this).scrollTop();
+		if (y > (window.innerHeight * 1.3)) {
+			$('#topMenu').fadeIn();
+		}
+		else {
+			$('#topMenu').fadeOut();
+		}
+	});
+
+	$("#flickrSearch").selectmenu({
+		change: function (event, data) {
+			loadFlickrImages(data.item.value)
+		}
+	});
+
+	loadFlickrImages('webservice');
 });
+
+function hideSubMenus() {
+	var subMenus = document.getElementsByClassName("subList");
+	var i = 0;
+
+	for(i = 0; i < subMenus.length; i++) {
+		subMenus[i].style.display = 'none';
+	}
+}
+
+function loadFlickrImages(search) {
+	$("#flickrPhotos").html('');
+
+	$.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3c195123945140651a8151c7d4cfbd0b&text=' + search + '&per_page=10&format=json&nojsoncallback=1', function (data) {
+		$.each( data.photos.photo, function (key, val) {
+			var baseLink = 'https://farm' + val.farm + '.staticflickr.com/' + val.server + '/' + val.id + '_' + val.secret;
+			$("#flickrPhotos").append('<a href="' + baseLink + '.jpg" class="flickrLink" target="_blank"><img src="' + baseLink + '_q.jpg" alt="hoi" /></a>');
+		});
+	});
+}
